e730210ab494a2a6526bea043ad7664444db49fa
Uladzimir Kulakou <rombik9@ya.ru>
2021-07-01 19:26:58 +0300

    commit 1

---
+++ NabelsHomeWork_15/NabelsHomeWork_15/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NabelsHomeWork_15
+//
+//  Created by Uladzimir Kulakou on 7/1/21.
+//  Copyright © 2021 Uladzimir Kulakou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
